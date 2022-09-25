ad7c596eca01c84325b53e171f596d325d43026c
matteobonvini <mbonvini@andrew.cmu.edu>
2022-09-25 10:56:39 -0400

    cate now returns pseudo.y

--- R/cate.R
+++ R/cate.R
@@ -33,11 +33,13 @@ cate <- function(v0, learner, y, a, x, v, nsplits = 5, foldid = NULL, ...) {
 
   est <- replicate(length(learner), array(NA, dim = c(nrow(v0), 3, nsplits)),
                    simplify = FALSE)
+  pseudo.y <- replicate(length(learner), matrix(NA, ncol = 1, nrow = n),
+                        simplify = FALSE)
   ites_v <- replicate(length(learner), matrix(NA, ncol = 3, nrow = n),
                       simplify = FALSE)
   ites_x <- replicate(length(learner), matrix(NA, ncol = 1, nrow = n),
                       simplify = FALSE)
-  names(est) <- names(ites_v) <- names(ites_x) <- learner
+  names(est) <- names( pseudo.y) <- names(ites_v) <- names(ites_x) <- learner
 
   if(any(learner == "lp-r")) {
     est[["lp-r"]] <- matrix(NA, ncol = 3, nrow = nrow(v0))
@@ -165,9 +167,10 @@ cate <- function(v0, learner, y, a, x, v, nsplits = 5, foldid = NULL, ...) {
         pseudo <- (a.te - pihat) / (pihat * (1 - pihat)) *
           (y.te - a.te * mu1hat - (1 - a.te) * mu0hat) + mu1hat - mu0hat
         est[[alg]][, , k] <- drl(y.tr = pseudo, x.tr = v.te, new.x = v0)
+        pseudo.y[[alg]][test.idx, 1] <- pseudo
         ites_v[[alg]][test.idx, ] <- drl(y.tr = pseudo, x.tr = v.te, new.x = v.te)
         ites_x[[alg]][test.idx, 1] <- drl.ite(y.tr = pseudo, x.tr = x.te,
-                                             new.x = x.te)
+                                              new.x = x.te)
 
       } else if(alg == "t"){
         if(all(colnames(x) %in% colnames(v))) {
@@ -201,7 +204,8 @@ cate <- function(v0, learner, y, a, x, v, nsplits = 5, foldid = NULL, ...) {
 
   out <- lapply(learner, function(w) apply(est[[w]], c(1, 2), mean))
 
-  ret <- list(est = out, fold_est = est, ites_v = ites_v, ites_x = ites_x)
+  ret <- list(est = out, fold_est = est, pseudo.y = pseudo.y, ites_v = ites_v,
+              ites_x = ites_x)
   return(ret)
 }
 
