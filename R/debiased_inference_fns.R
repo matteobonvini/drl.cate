47fd95d3daab9fbbc527ababca7dceb251406140
matteobonvini <mbonvini@andrew.cmu.edu>
2023-03-23 13:01:14 -0400

    fix documentation

--- R/debiased_inference_fns.R
+++ R/debiased_inference_fns.R
@@ -1,16 +1,16 @@
-# These functions are slight modifications of those contained in the R package 
+# These functions are slight modifications of those contained in the R package
 # for dose-response debiased inference available at
 # https://github.com/Kenta426/DebiasedDoseResponse
 # Main reference is https://arxiv.org/abs/2210.06448
 # Article and repository authored by Kenta Takatsu and Ted Westling.
 
 .compute.rinfl.func <- function(Y, A, a, h, b, debias, kern, muhat.mat, mhat.obs){
-  
+
   n <- length(A); bw.min <- sort(abs(A - a))[21]
   h <- max(h, bw.min); b <- max(b, bw.min)
   a.std.h <- (A - a)/h; kern.std.h <- kern(a.std.h)/h
   a.std.b <- (A - a)/b; kern.std.b <- kern(a.std.b)/b
-  
+
   # Compute 2x2 inverse matrix ------------------------------------------------
   c0.h <- mean(kern.std.h)
   c1.h <- mean(kern.std.h * a.std.h)
@@ -18,7 +18,7 @@
   Dh <- matrix(c(c0.h, c1.h,
                  c1.h, c2.h), nrow = 2)
   Dh.inv <- solve(Dh)
-  
+
   # Compute 4x4 inverse matrix ------------------------------------------------
   c0.b <- mean(kern.std.b)
   c1.b <- mean(kern.std.b * a.std.b)
@@ -32,7 +32,7 @@
                  c2.b, c3.b, c4.b, c5.b,
                  c3.b, c4.b, c5.b, c6.b), nrow = 4)
   Db.inv <- solve(Db)
-  
+
   # Estimate local linear components ------------------------------------------
   g2.h <- (A - a)/h
   int1.h <- colMeans(kern.std.h * (muhat.mat - mhat.obs))
@@ -41,7 +41,7 @@
   res.h <- Y - (gamma.h[1] + gamma.h[2] * a.std.h)
   inf.fn <- t(Dh.inv %*% rbind(res.h * kern.std.h + int1.h,
                                g2.h * res.h * kern.std.h + int2.h))
-  
+
   # Estimate local polynomial components --------------------------------------
   g2.b <- (A - a)/b
   g3.b <- ((A - a)/b)^2
@@ -56,12 +56,12 @@
                                       g2.b * res.b * kern.std.b + int2.b,
                                       g3.b * res.b * kern.std.b + int3.b,
                                       g4.b * res.b * kern.std.b + int4.b))
-  
+
   # Build the influence function ----------------------------------------------
   if(debias) {
     c2 <- integrate(function(u){u^2 * kern(u)}, -Inf, Inf)$value
   } else c2 <- 0
-  
+
   return (inf.fn[,1] - (h/b)^2 * c2 * inf.fn.robust[,3])
 }
 
@@ -116,24 +116,13 @@
   return(arg)
 }
 
-
-#' Title
-#'
-#' @param res.df
-#' @param ci
-#' @param unif
-#'
-#' @return
-#' @export
-#' @import ggplot2
-#' @examples
 plot_debiased_curve <- function(res.df, ci=TRUE, unif=TRUE){
   p <- ggplot2::ggplot(res.df) +
     ggplot2::geom_line(aes(x=eval.pts, y=theta)) +
     ggplot2::xlab("Exposure") +
     ggplot2::ylab("Covariate-adjusted outcome") +
     ggplot2::theme_minimal()
-  
+
   if(ci){
     p <- p + ggplot2::geom_pointrange(aes(x=eval.pts, y=theta,
                                           ymin=ci.ll.pts,
@@ -156,7 +145,7 @@ plot_debiased_curve <- function(res.df, ci=TRUE, unif=TRUE){
 
 
 
-debiased_inference <- function(A, pseudo.out, muhat.mat, mhat.obs, debias, 
+debiased_inference <- function(A, pseudo.out, muhat.mat, mhat.obs, debias,
                                tau=1, eval.pts=NULL, ...){
   # Parse control inputs ------------------------------------------------------
   control <- .parse.debiased_inference(...)
@@ -170,7 +159,7 @@ debiased_inference <- function(A, pseudo.out, muhat.mat, mhat.obs, debias,
   if (is.null(eval.pts)){
     eval.pts <- seq(quantile(A, 0.05), quantile(A, 0.95),  length.out=30)
   }
-  
+
   # Compute bandwidth ---------------------------------------------------------
   if(control$bandwidth.method == "LOOCV"){
     if (is.null(control$bw.seq)){
@@ -206,11 +195,11 @@ debiased_inference <- function(A, pseudo.out, muhat.mat, mhat.obs, debias,
                         bwselect="imse-dpi")$bws[,2]
     b.opt <- h.opt/tau
   }
-  
+
   # Fit debiased local linear regression --------------------------------------
   est.res <- .lprobust(A, pseudo.out, h.opt, b.opt,
                        debias, eval=eval.pts, kernel.type=kernel.type)
-  
+
   # Estimate influence function sequence --------------------------------------
   rinf.fns <- mapply(function(a, h.val, b.val){
     .compute.rinfl.func(pseudo.out, A, a, h.val, b.val, debias, kern,
@@ -220,7 +209,7 @@ debiased_inference <- function(A, pseudo.out, muhat.mat, mhat.obs, debias,
   alpha <- control$alpha.pts
   ci.ll.p <- est.res[,"theta.hat"]-qnorm(1-alpha/2)*rif.se
   ci.ul.p <- est.res[,"theta.hat"]+qnorm(1-alpha/2)*rif.se
-  
+
   # Compute uniform bands by simulating GP ------------------------------------
   if(control$unif){
     get.unif.ep <- function(alpha){
@@ -238,7 +227,7 @@ debiased_inference <- function(A, pseudo.out, muhat.mat, mhat.obs, debias,
   else{
     ci.ll.u <- ci.ul.u <- 0
   }
-  
+
   # Output data.frame ---------------------------------------------------------
   res <- data.frame(
     eval.pts=eval.pts,
@@ -268,7 +257,7 @@ debiased_inference <- function(A, pseudo.out, muhat.mat, mhat.obs, debias,
   } else {
     c.2 <- 0
   }
-  
+
   e1 <- matrix(c(1, 0), ncol=2); e3 <- matrix(c(1, 0, 0, 0), ncol=4)
   w.h.zero   <- .kern(0, kernel.type)/h
   w.b.zero   <- .kern(0, kernel.type)/b
@@ -297,21 +286,21 @@ debiased_inference <- function(A, pseudo.out, muhat.mat, mhat.obs, debias,
 
 
 # Perform leave-one-out cross-validation based on the "hat matrix trick"
-.robust.loocv <- function(x, y, h, b, debias, eval.pt=NULL, 
+.robust.loocv <- function(x, y, h, b, debias, eval.pt=NULL,
                           kernel.type="epa"){
   ind <- order(x); x <- x[ind]; y <- y[ind]
   if (is.null(eval.pt)){
     eval.pt <- seq(quantile(x, 0.05), quantile(x, 0.95), length.out=30)
   }
-  
+
   # Compute hat matrix --------------------------------------------------------
-  hat.val <- .hatmatrix(x, y, h, b, debias, eval.pt=eval.pt, 
+  hat.val <- .hatmatrix(x, y, h, b, debias, eval.pt=eval.pt,
                         kernel.type=kernel.type)
-  
+
   # Compute debiased local linear estimator -----------------------------------
   est <- .lprobust(x, y, h, b, debias, eval.pt=eval.pt, kernel.type=kernel.type)
   est.fn <- approx(eval.pt, est[,"theta.hat"], xout = x)$y
-  
+
   # Compute the estimate of LOOCV risk ----------------------------------------
   mean(((y - est.fn) / (1 - hat.val))^2, na.rm=TRUE)
 }
